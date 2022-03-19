51ea249c0dea2a02a4fba80c6023cfb176f58ad7
Ovan Zaimul <ovan@softwareseni.com>
2022-03-19 20:55:39 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [time, setTime] = useState(0);
+	const [isStart, setIsStart] = useState(false);
+	const [laps, setLaps] = useState<number[]>([]);
+
+	useEffect(() => {
+		let timeId: any;
+		if (isStart) {
+			timeId = window.setInterval(() => {
+				setTime(prevtime => prevtime + 1);
+			}, 100);
+		}
+
+		return () => {
+			console.log('clear');
+			clearInterval(timeId);
+		};
+	}, [isStart]);
+
+	return (
+		<div className='App'>
+			<h1>{time}</h1>
+			<div>
+				{isStart && time > 0 && (
+					<button type='button' onClick={() => setLaps([...laps, time])}>
+						Lap
+					</button>
+				)}
+				{isStart && <button onClick={() => setIsStart(false)}>pause</button>}
+				{!isStart && time > 0 && <button onClick={() => setIsStart(true)}>resume</button>}
+				{!isStart && time === 0 && <button onClick={() => setIsStart(true)}>start</button>}
+				{!isStart && time > 0 && (
+					<button
+						onClick={() => {
+							setLaps([]);
+							setTime(0);
+						}}
+					>
+						reset
+					</button>
+				)}
+			</div>
+			<ul style={{ width: '500px', margin: '20px auto' }}>
+				{laps.map((lap, i) => (
+					<li key={i}>{lap}</li>
+				))}
+				{isStart && laps.length === 0 && <p>No laps yet</p>}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
